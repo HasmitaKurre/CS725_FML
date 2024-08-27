dace1f0f7eae590b1965e3af70115c7bb0a1ce6f
Hasmita Kurre <61547475+HasmitaKurre@users.noreply.github.com>
2024-08-27 22:14:15 +0530

    Update part1B.py

--- Assignment_1/part1B.py
+++ Assignment_1/part1B.py
@@ -1,23 +1,42 @@
+from matplotlib import pyplot as plt
 import numpy as np
+import random
+random.seed(45)
 
-def initialise_input(N, d):
-  '''
-  N: Number of vectors
-  d: dimension of vectors
-  '''
-  np.random.seed(0)
-  U = np.random.randn(N, d)
-  M1 = np.abs(np.random.randn(d, d))
-  M2 = np.abs(np.random.randn(d, d))
+num_coins = 100
+def toss(num_trials):
+    '''
+    num_trials: number of trials to be performed.
+    
+    return a numpy array of size num_trials with each entry representing the number of heads found in each trial
 
-  return U, M1, M2
+    Use for loops to generate the numpy array and 'random.choice()' to simulate a coin toss
+    
+    NOTE: Do not use predefined functions to directly get the numpy array. 
+    '''
+    global num_coins
+    results = []
+    
+    ## Write your code here
 
-def solve(N, d):
-  U, M1, M2 = initialise_input(N, d)
+    return results
+    
 
-  '''
-  Enter your code here for steps 1 to 6
-  '''
-  return max_indices
-  
-solve(N,d)
+def plot_hist(trial):
+    '''
+    trial: vector of values for a particular trial.
+
+    plot the histogram for each trial.
+    Use 'axs' from plt.subplots() function to create histograms. You can search about how to use it to plot histograms.
+
+    Save the images in a folder named "histograms" in the current working directory.  
+    '''
+    fig, axs = plt.subplots(figsize =(10, 7), tight_layout=True)
+    
+    ## Write your code here
+
+if __name__ == "__main__":
+    num_trials_list = [10,100,1000,10000,100000]
+    for num_trials in num_trials_list:
+        heads_array = toss(num_trials)
+        plot_hist(heads_array)
